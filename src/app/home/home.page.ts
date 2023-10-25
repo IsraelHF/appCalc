f10f8b4914956ab31dddfba96f618b3e61f4d244
IsraelHF <israel.com.ihf80@gmail.com>
2023-10-25 11:01:46 -0300

    variável resultadoAnterior

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,67 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor : string = '0';
-  valor1! : number;
-  valor2! : number;
-  operacao! : number;
+  visor: string = '0';
+  valor1!: number;
+  valor2!: number;
+  operacao!: number;
+  resultadoAnterior!: number;
   constructor() {}
 
-  adicionarNumero(valor: string){
-    if((this.visor.length == 1) && (this.visor == '0')){
-    this.visor = valor;
-  }else{
-    this.visor += valor;
+  adicionarNumero(valor: string) {
+    if (this.visor.length == 1 && this.visor == '0') {
+      this.visor = valor;
+    } else {
+      this.visor += valor;
     }
   }
 
-  adicionarPonto(valor: string){
-    this.visor += valor;
+  adicionarPonto(valor: string) {
+    if (this.visor.indexOf('.') === -1) {
+      this.visor += valor;
+    }
   }
 
-  adicionarOperacao(valor : number){
+  adicionarOperacao(valor: number) {
+    if (this.valor1 === null) {
+      this.valor1 = +this.visor;
+    } else {
+      this.calcular();
+    }
     this.operacao = valor;
-    this.valor1 = +this.visor;
-    /*this.valor1 = Number(this.visor);  Outra forma de transformar string em number */
-    this.zerar();
+    /*this.valor1 = Number(this.visor); outra maneira de transformar string em number */
+    this.visor = '0';
+    //this.zerar();
   }
 
-  porcentagem(){
+  porcentagem() {
     this.valor1 = +this.visor;
-    this.visor = "" + (this.valor1 /100);
+    this.visor = '' + this.valor1 / 100;
   }
 
   negativo() {
     this.valor1 = +this.visor;
     this.visor = '' + this.valor1 * -1;
   }
-  
-  calcular(){
+
+  calcular() {
     this.valor2 = +this.visor;
-    switch(this.operacao){
-      case 0 : {
-        this.visor = "" + (this.valor1 + this.valor2);
+    switch (this.operacao) {
+      case 0: {
+        this.visor = '' + (this.valor1 + this.valor2);
         break;
       }
-      case 1 : {
-        this.visor = "" + (this.valor1 - this.valor2);
-        break;
-      }
-
-      case 2 : {
-        this.visor = "" + (this.valor1 * this.valor2);
+      case 1: {
+        this.visor = '' + (this.valor1 - this.valor2);
         break;
       }
 
-      case 3 : {
-        this.visor = "" + (this.valor1 / this.valor2);
+      case 2: {
+        this.visor = '' + this.valor1 * this.valor2;
+        break;
+      }
+
+      case 3: {
+        this.visor = '' + this.valor1 / this.valor2;
         break;
       }
     }
   }
 
-  zerar(){
+  zerar() {
     this.visor = '0';
   }
-  
 }
